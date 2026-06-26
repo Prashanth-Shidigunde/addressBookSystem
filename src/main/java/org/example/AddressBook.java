b045118d01de67fbfb60d062e3931701ed4eb3a4
Prashanth-Shidigunde <shidigundeprashanth@gmail.com>
2026-06-26 16:45:46 +0530

    "UC3 ability to edit exisiting contact to addessBook "

--- src/main/java/org/example/AddressBook.java
+++ src/main/java/org/example/AddressBook.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddressBook {
 
@@ -8,6 +9,29 @@ public class AddressBook {
 
     public void addContact(Contact contact) {
         contacts.add(contact);
+    }
+    //UC2 edit data
+    public boolean editContact(String firstName, Contact updatedContact) {
+
+        // Traverse through all contacts
+        for (int i = 0; i < contacts.size(); i++) {
+
+            Contact existingContact = contacts.get(i);
+
+            // Check whether the contact exists
+            if (existingContact.getFirstName().equalsIgnoreCase(firstName)) {
+
+                // Replace old contact with updated contact
+                contacts.set(i, updatedContact);
+                return true;
+            }
+        }
+
+        // Contact not found
+        return false;
+    }
+    public List<Contact> getContacts(){
+        return contacts;
     }
 
     public void displayContacts() {
--- src/test/java/org/example/AddressBookTest.java
+++ src/test/java/org/example/AddressBookTest.java
@@ -27,5 +27,44 @@ class AddressBookTest {
         assertEquals("Prashanth", addressBook.contacts.getFirst().firstName);
         assertEquals("S", addressBook.contacts.getFirst().lastName);
     }
+    @Test
+    void shouldEditExistingContact() {
+
+        // Create AddressBook object
+        AddressBook addressBook = new AddressBook();
+
+        // Create existing contact
+        Contact oldContact = new Contact(
+                "Prashanth",
+                "S",
+                "Old Address",
+                "Bangalore",
+                "Karnataka",
+                "560001",
+                "9876543210",
+                "old@gmail.com"
+        );
+        // Add contact to AddressBook
+        addressBook.addContact(oldContact);
+
+        // Create updated contact details
+        Contact updatedContact = new Contact(
+                "Prashanth",
+                "S",
+                "New Address",
+                "Mysore",
+                "Karnataka",
+                "570001",
+                "9999999999",
+                "new@gmail.com"
+        );
+        // Edit contact
+        boolean result = addressBook.editContact("Prashanth", updatedContact);
+
+        // Verify contact is updated successfully
+        assertTrue(result);
+        assertEquals("New Address", addressBook.getContacts().getFirst().getAddress());
+        assertEquals("Mysore", addressBook.getContacts().getFirst().getCity());
+    }
 
 }
